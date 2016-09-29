b15e05223343bb62fa2585aefd25f0ad5f95cc72
Simon Bihel <simon.bihel@outlook.com>
2016-09-29 16:50:03 +0200

    tictacforever

--- tp2/tictacforever.c
+++ tp2/tictacforever.c
@@ -1,21 +1,33 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <setjmp.h>
 
 int state = 0;
+jmp_buf env;
 
 void alarm_handler(int sig) {
   printf("%d\n", state);
   alarm(1);
 }
 
+void interupt_handler(int sig) {
+  state = 0;
+  longjmp(env, 12345789);
+  // appenrently there's no need to unblock/unmask the signal
+}
+
 int main() {
   signal(SIGALRM, alarm_handler);
+  signal(SIGINT, interupt_handler);
   alarm(1);
-  
+
+  setjmp(env);
+
   do {
     state++;
+    usleep(10000);
   } while(1);
-  
+
   return 0;
 }
