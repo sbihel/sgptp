1f69bb5be27eb16ed7ff468c055f971fed892b1d
Louis.Beziaud <louis.beziaud@ens-rennes.fr>
2016-11-19 01:28:11 +0100

    added an example of how to pass argc argv to thread

--- nachos/test/hello.c
+++ nachos/test/hello.c
@@ -10,10 +10,48 @@
 #include "userlib/syscall.h"
 #include "userlib/libnachos.h"
 
-int
-main()
-{
-  n_printf("** ** ** Bonjour le monde ** ** **\n");
+typedef void (*VoidFunctionPtr)();
+
+typedef struct {
+  VoidFunctionPtr func;
+  int argc;
+  char **argv;
+} FunArgs;
+
+static void threadStart(int fa) {
+  VoidFunctionPtr func = ((FunArgs*)fa)->func;
+  int argc = ((FunArgs*)fa)->argc;
+  char **argv = ((FunArgs*)fa)->argv;
+
+  VoidFunctionPtr func2;
+  func2 = (VoidFunctionPtr)func;
+  func2(argc, argv);
+
+  Exit(0);
+}
+
+ThreadId threadCreate2(char *debug_name, VoidFunctionPtr func, int argc, char *argv[]) {
+  FunArgs fa;
+  fa.func = func;
+  fa.argc = argc;
+  fa.argv = argv;
+
+  return newThread(debug_name, (int)threadStart, (int)(&fa));
+}
+
+void dump_args(int argc, char *argv[]) {
+  int i;
+  n_printf("%d\n", argc);
+  for(i = 0; i < argc; i++) {
+    n_printf("%s ", argv[i]);
+  }
+  n_printf("\n");
+}
+
+int main() {
+  char *argv[3] = {"Bonjour", "le", "monde"};
+
+  threadCreate2("dump_args", dump_args, 3, argv);
 
   return 0;
 }
