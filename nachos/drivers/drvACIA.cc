e3c941e83a1f21f18ae9cc381be3d25eabc84f55
Simon Bihel <simon.bihel@outlook.com>
2016-11-24 15:35:39 +0100

    Fix busy waiting

--- nachos/drivers/drvACIA.cc
+++ nachos/drivers/drvACIA.cc
@@ -1,8 +1,8 @@
 /* \file drvACIA.cc
    \brief Routines of the ACIA device driver
 //
-//      The ACIA is an asynchronous device (requests return 
-//      immediately, and an interrupt happens later on).  
+//      The ACIA is an asynchronous device (requests return
+//      immediately, and an interrupt happens later on).
 //      This is a layer on top of the ACIA.
 //      Two working modes are to be implemented in assignment 2:
 //      a Busy Waiting mode and an Interrupt mode. The Busy Waiting
@@ -11,8 +11,8 @@
 //      roadmap for further details).
 //
 //  Copyright (c) 1999-2000 INSA de Rennes.
-//  All rights reserved.  
-//  See copyright_insa.h for copyright notice and limitation 
+//  All rights reserved.
+//  See copyright_insa.h for copyright notice and limitation
 //  of liability and disclaimer of warranty provisions.
 //
 */
@@ -27,10 +27,10 @@
 //-------------------------------------------------------------------------
 // DriverACIA::DriverACIA()
 /*! Constructor.
-  Initialize the ACIA driver. In the ACIA Interrupt mode, 
-  initialize the reception index and semaphores and allow 
-  reception and emission interrupts. 
-  In the ACIA Busy Waiting mode, simply inittialize the ACIA 
+  Initialize the ACIA driver. In the ACIA Interrupt mode,
+  initialize the reception index and semaphores and allow
+  reception and emission interrupts.
+  In the ACIA Busy Waiting mode, simply inittialize the ACIA
   working mode and create the semaphore.
   */
 //-------------------------------------------------------------------------
@@ -42,7 +42,8 @@ DriverACIA::DriverACIA()
   receive_sema = new Semaphore((char*)"receive_sema driver", BUFFER_SIZE);
   ind_send     = 0;
   ind_rec      = 0;
-  g_machine->acia->SetWorkingMode(REC_INTERRUPT | SEND_INTERRUPT);
+  //g_machine->acia->SetWorkingMode(REC_INTERRUPT | SEND_INTERRUPT);
+  g_machine->acia->SetWorkingMode(BUSY_WAITING);
 #else
   printf("**** Warning: contructor of the ACIA driver not implemented yet\n");
   exit(-1);
@@ -56,7 +57,7 @@ DriverACIA::DriverACIA()
 //-------------------------------------------------------------------------
 
 int DriverACIA::TtySend(char* buff)
-{ 
+{
 #ifdef ETUDIANTS_TP
   send_sema->P();
 
@@ -81,7 +82,7 @@ int DriverACIA::TtySend(char* buff)
 
 //-------------------------------------------------------------------------
 // DriverACIA::TtyReceive(char* buff,int length)
-/*! Routine to reveive a message through the ACIA 
+/*! Routine to reveive a message through the ACIA
 //  (Busy Waiting and Interrupt mode).
   */
 //-------------------------------------------------------------------------
@@ -115,29 +116,35 @@ int DriverACIA::TtyReceive(char* buff,int lg)
 //-------------------------------------------------------------------------
 // DriverACIA::InterruptSend()
 /*! Emission interrupt handler.
-  Used in the ACIA Interrupt mode only. 
+  Used in the ACIA Interrupt mode only.
   Detects when it's the end of the message (if so, releases the send_sema semaphore), else sends the next character according to index ind_send.
   */
 //-------------------------------------------------------------------------
 
 void DriverACIA::InterruptSend()
 {
+#ifdef ETUDIANTS_TP
+#else
   printf("**** Warning: send interrupt handler not implemented yet\n");
   exit(-1);
+#endif
 }
 
 //-------------------------------------------------------------------------
 // DriverACIA::Interrupt_receive()
 /*! Reception interrupt handler.
-  Used in the ACIA Interrupt mode only. Reveices a character through the ACIA. 
-  Releases the receive_sema semaphore and disables reception 
-  interrupts when the last character of the message is received 
+  Used in the ACIA Interrupt mode only. Reveices a character through the ACIA.
+  Releases the receive_sema semaphore and disables reception
+  interrupts when the last character of the message is received
   (character '\0').
   */
 //-------------------------------------------------------------------------
 
 void DriverACIA::InterruptReceive()
 {
+#ifdef ETUDIANTS_TP
+#else
   printf("**** Warning: receive interrupt handler not implemented yet\n");
   exit(-1);
+#endif
 }
