1f45b6f180f85e934e6c4a7403d68afb29e47a32
Louis.Beziaud <louis.beziaud@ens-rennes.fr>
2017-01-30 17:10:57 +0100

    add shell exit command

--- nachos/test/shell.c
+++ nachos/test/shell.c
@@ -52,7 +52,8 @@ main()
 	    buffer[++i]='\0';
 	    
 	} else {buffer[++i] = '\0';}
-	
+
+	if (n_strcmp("exit", buffer) == 0) break;
 		
 	// Execute the command
 	// In the case it is a background command, don't wait for its completion
