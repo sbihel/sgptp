a490b0cd0d37ac8a0fbe0568b56fb992d70c2549
Louis.Beziaud <louis.beziaud@ens-rennes.fr>
2017-02-11 23:54:26 +0100

    fixed #17 by removing non-working stack struct

--- nachos/test/prodcons2.c
+++ nachos/test/prodcons2.c
@@ -1,97 +1,72 @@
 #include "userlib/syscall.h"
 #include "userlib/libnachos.h"
 
-#define STACK_MAX 10
+#define MAX 10
+char queue[MAX];
+int head = 0, tail = 0;
+SemId nchars, nholes;
+LockId mutex;
 
-struct Stack {
-  int data[STACK_MAX];
-  int size;
-};
-typedef struct Stack Stack;
+void producer(void) {
+  while (1) {
+    P(nholes);
+    LockAcquire(mutex);
 
-void Stack_Init(Stack *S) {
-  S->size = 0;
+    if (queue[head] == 'P') {
+      n_printf("unconsumed!\n");
+    }
+    n_printf("produce\n");
+    queue[head] = 'P';
+    head = (head + 1) % MAX;
+
+    LockRelease(mutex);
+    V(nchars);
+  }
 }
 
-void Stack_Push(Stack *S, int d) {
-  if (S->size < STACK_MAX)
-    S->data[S->size++] = d;
-  else
-    n_printf("Error: stack full\n");
+void consumer(void) {
+  while (1) {
+    P(nchars);
+    LockAcquire(mutex);
+
+    if (queue[tail] == 'C') {
+      n_printf("unproduced!\n");
+    }
+    n_printf("consume\n");
+    queue[tail] = 'C';
+    tail = (tail + 1) % MAX;
+
+    LockRelease(mutex);
+    V(nholes);
+  }
 }
-
-void Stack_Pop(Stack *S) {
-  if (S->size == 0)
-    n_printf("Error: stack empty\n");
-  else
-    S->size--;
-}
-
-Stack *S;
-
-SemId fillCount;
-SemId emptyCount;
-
-LockId buffer_mutex;
-
-#define NUM_CONSUMERS 1
-#define NUM_PRODUCERS 1
-
-void producer();
-void consumer();
 
 int main() {
-  fillCount = SemCreate("fillCount", 0);
-  emptyCount = SemCreate("emptyCount", STACK_MAX);
+  nchars = SemCreate("nchars", 0);
+  nholes = SemCreate("nholes", MAX);
 
-  buffer_mutex = LockCreate("buffer_mutex");
+  mutex = LockCreate("mutex");
 
-  Stack_Init(S);
-
-  ThreadId consumers[NUM_CONSUMERS], producers[NUM_PRODUCERS];
+  ThreadId consumers[10], producers[10];
 
   int i;
   char str[20];
-  for (i = 0; i < NUM_PRODUCERS; i++) {
+  for (i = 0; i < 10; i++) {
     n_snprintf(str, 20, "prod%d", i);
     producers[i] = threadCreate(str, producer);
-  }
-  for (i = 0; i < NUM_CONSUMERS; i++) {
     n_snprintf(str, 20, "cons%d", i);
     consumers[i] = threadCreate(str, consumer);
   }
 
-  for (i = 0; i < NUM_PRODUCERS; i++) {
+  for (i = 0; i < 10; i++) {
     Join(producers[i]);
-  }
-  for (i = 0; i < NUM_CONSUMERS; i++) {
     Join(consumers[i]);
   }
 
-  SemDestroy(fillCount);
-  SemDestroy(emptyCount);
+  SemDestroy(nchars);
+  SemDestroy(nholes);
 
-  LockDestroy(buffer_mutex);
+  LockDestroy(mutex);
 
   return 0;
-}
-
-void producer() {
-  while (1) {
-    P(emptyCount);
-    LockAcquire(buffer_mutex);
-    Stack_Push(S, 0);
-    LockRelease(buffer_mutex);
-    V(fillCount);
-  }
-}
-
-void consumer() {
-  while (1) {
-    P(fillCount);
-    LockAcquire(buffer_mutex);
-    Stack_Pop(S);
-    LockRelease(buffer_mutex);
-    V(emptyCount);
-  }
 }
